fef2b23348370813f1900586d45edfb7e89e0198
JE-1 <jedde123@gmail.com>
2024-05-27 22:36:28 +0300

    second commit

---
+++ (F7) Djibouti Rt Analysis.py
@@ -0,0 +1,109 @@
+
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+plt.cla()
+plt.clf()
+
+n=600
+gammar=1/14
+gammarv=gammar
+gammad=gammar/50
+gammadv=gammad/5
+Tvi=180
+Tii=360
+sigma=1/5.2
+
+vi=0.9
+   
+e=0.55
+v=0.015
+
+for p in range(1,10):
+
+        Rt=1+(p-1)*0.125
+
+        for qq in range(5) :
+            tau=qq/4
+
+            R=0.0764
+            D=0.0002
+            Iv=0
+            I=0.0018
+            Sv=0
+            Sn=0.0233
+            E=I
+            Ev=0
+            S=1-R-D-I-Iv-Sv-Sn-E-Ev
+
+            yD=[]
+            x=[]        
+                
+            for i in range(n):
+                
+                
+                beta=Rt*gammar
+                betav=Rt*gammarv
+                
+                Si=S
+                Svi=Sv
+                Sni=Sn
+                Ei=E
+                Evi=Ev
+                Ii=I
+                Ivi=Iv
+                Ri=R
+                Di=D
+
+                x.append(i)
+                yD.append(Di*100)
+
+        
+                S= -beta*(Ii+Ivi)*Si - (1-tau)*v*Si + Si
+                Sv= (1-tau)*(1-e*vi)*v*Si - betav*(Ii+Ivi)*Svi + 2*tau*v*Sni*0.1 + Svi
+                Sn= (gammarv*Ivi+gammar*Ii)/Tii + ((1-tau)*e*vi*v*Si+2*tau*v*Sni*e*vi)/Tvi - betav*(Ii+Ivi)*Sni - 2*tau*v*Sni + Sni
+                E= beta*(Ii+Ivi)*Si - sigma*Ei + Ei
+                Ev= betav*(Ii+Ivi)*(Svi+Sni) - sigma*Evi + Evi
+                I= sigma*Ei - (gammad + gammar)*Ii + Ii
+                Iv=  sigma*Evi - (gammadv + gammarv)*Ivi + Ivi
+                R= (1-tau)*e*vi*v*Si + 2*tau*v*Sni*e + gammar*Ii + gammarv*Ivi - ((1-tau)*e*vi*v*Si+2*tau*v*Sni*e*vi)/Tvi - (gammar*Ii+gammarv*Ivi)/Tii + Ri
+                D= gammad*Ii + gammadv*Ivi + Di
+
+                
+
+
+            k=yD
+            yaxis= 'Cumulative Deaths (%)'
+
+            plt.subplot(3,3,p)
+                        
+            title=['tau=',str(tau)]
+            a=''.join(title)
+            plt.plot(x,k,label=a)
+
+            plt.legend()
+            plt.xlabel('Days')
+            plt.ylabel(yaxis)
+
+            if qq==0:
+                ymin=yD[250]-0.05
+
+            if qq==4:
+                ymax=yD[599]+0.05
+
+                plt.xlim((0,600))
+                plt.ylim((0,ymax))
+
+            
+            b=['Rt=',str(Rt),' / e=',str(e),' / v=',str(v)]
+            b=''.join(b)
+            plt.title(b)
+
+plt.subplots_adjust(left=0.05, bottom=0.071, right=0.988, top=0.962, wspace=0.238, hspace=0.471)
+plt.gcf().set_size_inches(16,8)
+
+plt.savefig('C:/Users/user/Desktop/publication figures/Djibouti Rt sensitivity.jpeg',dpi=400)
+
+plt.show()
+
